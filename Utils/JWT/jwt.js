bacb8ff6346a9220add984736d48a2cf021ad42b
abdirisaq874 <78967841+abdirisaq874@users.noreply.github.com>
2023-10-31 09:12:35 +0300

    jwtfile modification

--- Utils/JWT/jwt.js
+++ Utils/JWT/jwt.js
@@ -23,7 +23,7 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
   });
 
   res.cookie('refreshToken', refreshTokenJWT, {
-    httpOnly: true,
+    // httpOnly: true,
     secure: true, //process.env.NODE_ENV === 'production',
     signed: true,
     sameSite: 'none',
